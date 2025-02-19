b2e872c2dd3e98acf9a685187c44af66c8282d9b
Akmal khan <akmalkhann0@gmail.com>
2025-02-19 23:59:50 +0530

    add main running logics

--- tree/test-tree.c
+++ tree/test-tree.c
@@ -1,13 +1,81 @@
 #include "tree.c"
+#include <conio.h>
 
-void main(){
+void main()
+{
   tree t;
   init(&t);
-  insert(&t,10);
-  insert(&t,11);
-  insert(&t,18);
-  insert(&t,6);
-  insert(&t,9);
-  insert(&t,100);
-  display(t.root);
+  int choice, item;
+  node *ptr;
+  while (1)
+  {
+    system("cls");
+    puts("1. INSERT");
+    puts("2. PREORDER");
+    puts("3. INORDER");
+    puts("4. POSTORDER");
+    puts("5. MIRROR");
+    puts("6. FIND MINIMUM");
+    puts("7. FIND MAXIMUM");
+    puts("8. SEARCH");
+    puts("9. NODE COUNT");
+    puts("10. FULLNODE COUNT");
+    puts("11. LEAFNODE COUNT");
+    puts("12. NON LEAFNODE COUNT");
+    puts("13. HEIGHT");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+      printf("Enter a Number: ");
+      scanf("%d", &item);
+      insert(&t, item);
+      break;
+    case 2:
+      preorder(t.root);
+      break;
+    case 3:
+      inorder(t.root);
+      break;
+    case 4:
+      postorder(t.root);
+      break;
+    case 5:
+      mirror(t.root);
+      break;
+    case 6:
+      ptr = minnode(t.root);
+      (!ptr) ? printf("the tree is empty") : printf("the minimum item is %d", ptr->info);
+      break;
+    case 7:
+      ptr = maxnode(t.root);
+      (!ptr) ? printf("the tree is empty") : printf("the maximum item is %d", ptr->info);
+      break;
+    case 8:
+      printf("Enter the item to Search: ");
+      scanf("%d", &item);
+      ptr = search(t.root, item);
+      (!ptr) ? printf("item not found") : printf("the item %d is found", ptr->info);
+      break;
+    case 9:
+      printf("total node in tree is: %d", nodecount(t.root));
+      break;
+    case 10:
+      printf("total full node in tree is: %d", fullnodec(t.root));
+      break;
+    case 11:
+      printf("total leaf node in tree is: %d", leafnode(t.root));
+      break;
+    case 12:
+      printf("Total non leaf node or parent node is: %d", nonleafnode(t.root));
+      break;
+    case 13:
+      printf("the height of the tree is %d", height(t.root));
+      break;
+    default:
+      exit(1);
+    }
+    getch();
+  }
 }
--- tree/tree.c
+++ tree/tree.c
@@ -18,7 +18,23 @@ node * root;
 // all funtion declaration
 void init(tree * tptr);
 void insert(tree * tp,itype item);
+void mirror(node * ptr);
+void preorder(node * ptr);
+void inorder(node * ptr);
+void postorder(node * ptr);
+
+int nodecount(node * ptr);
+int fullnodec(node * ptr);
+int leafnode(node * ptr);
+int nonleafnode(node * ptr);
+int height(node * ptr);
+
+node * search(node * ptr,itype item);
+node * maxnode(node * ptr);
+node * minnode(node * ptr);
 node * create(itype item);
+itype delete(tree * tp,itype item);
+
 
 
 void init(tree * tptr){
@@ -41,9 +57,6 @@ void insert(tree * tp,itype item){
         }
         gptr = gptr->lptr;
       }
-      else if(gptr->info == ptr->info){
-        return;
-      }
       else{
         if(gptr->rptr == NULL){
           gptr->rptr = ptr;
@@ -56,14 +69,116 @@ void insert(tree * tp,itype item){
 }
 
 
-void display(node * ptr){
+void preorder(node * ptr){
   if(ptr){
     printf("%d ",ptr->info); 
-    if(ptr->lptr)
-      display(ptr->lptr);
-    if(ptr->rptr)
-      display(ptr->rptr);
+    preorder(ptr->lptr);
+    preorder(ptr->rptr);
   }
+}
+void inorder(node * ptr){
+  if(ptr){
+    inorder(ptr->lptr);
+    printf("%d ",ptr->info); 
+    inorder(ptr->rptr);
+  }
+}
+void postorder(node * ptr){
+  if(ptr){
+    postorder(ptr->lptr);
+    postorder(ptr->rptr);
+    printf("%d ",ptr->info); 
+  }
+}
+
+void mirror(node * ptr){
+  if(ptr){
+    node * temp = ptr->lptr;
+    ptr->lptr = ptr->rptr;
+    ptr->rptr = temp;
+    mirror(ptr->lptr);
+    mirror(ptr->rptr);
+  }
+}
+
+int nodecount(node * ptr){
+  if(ptr == NULL)
+    return 0;
+  else
+    return 1+nodecount(ptr->lptr)+nodecount(ptr->rptr);
+}
+
+int fullnodec(node * ptr){
+  if((ptr == NULL) || (ptr->lptr == ptr->rptr))
+    return 0;
+  else if (ptr->lptr != NULL && ptr->rptr != NULL)
+    return 1+fullnodec(ptr->lptr)+fullnodec(ptr->rptr);
+  else
+    return fullnodec(ptr->lptr)+fullnodec(ptr->rptr);
+}
+
+int leafnode(node * ptr){
+  if(ptr == NULL)
+    return 0;
+  else if(ptr->lptr == ptr->rptr)
+    return 1;
+  else
+    return leafnode(ptr->lptr)+leafnode(ptr->rptr);
+}
+
+int nonleafnode(node * ptr){
+  if(ptr == NULL || ptr->lptr == ptr->rptr)
+    return 0;
+  else if(ptr->lptr|| ptr->rptr)
+    return 1+nonleafnode(ptr->lptr)+nonleafnode(ptr->rptr);
+}
+
+// int height(node * ptr){
+//   int l,r;
+//   if(ptr == NULL || ptr->lptr == ptr->rptr)
+//     return 0;
+//   l = height(ptr->lptr);
+//   r = height(ptr->rptr);
+//   return 1+(r>l)?r:l;
+// }
+
+int height(node * ptr){
+  if(!ptr || ptr->lptr == ptr->rptr)
+    return -1;
+  else{
+    int l = height(ptr->lptr),r = height(ptr->rptr);
+    return 1+(l>r)?l:r;
+  }
+}
+
+node * minnode(node * ptr){
+  if(ptr == NULL)
+    return NULL;
+  else if(ptr->lptr == NULL)
+    return ptr;
+  else 
+    return minnode(ptr->lptr);
+}
+
+node * maxnode(node * ptr){
+  if(ptr == NULL)
+    return NULL;
+  else if(ptr->rptr == NULL)
+    return ptr;
+  else 
+    return maxnode(ptr->rptr);
+}
+
+node * search(node * ptr,itype item){
+  if(ptr){
+    if(ptr->info == item)
+      return ptr;
+    else if(ptr->info > item)
+      return search(ptr->lptr,item);
+    else
+      return search(ptr->rptr,item);
+  }
+  return NULL;
 }
 
 node * create(itype item){
